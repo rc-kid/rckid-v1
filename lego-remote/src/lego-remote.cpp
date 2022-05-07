175a1214c70de241a57bc8c32df5fc1f74da402c
zduka <zduka@outlook.com>
2022-05-07 22:55:04 +0200

    Some more code.

--- lego-remote/src/lego-remote.cpp
+++ lego-remote/src/lego-remote.cpp
@@ -45,13 +45,14 @@ constexpr gpio::Pin XR2_PIN = 5;
 constexpr gpio::Pin MR1_PIN = 6;
 constexpr gpio::Pin MR2_PIN = 7;
 
-NRF24L01 radio{NRF_CS_PIN, NRF_RXTX_PIN};
+NRF24L01 nrf_{NRF_CS_PIN, NRF_RXTX_PIN};
 
 NeopixelStrip<5> leds{NEOPIXEL_PIN};
 
 volatile struct {
     bool tick : 1;
     uint8_t tickCounter : 2;
+    bool radioIrq : 1;
 } status_;
 
 enum class OutputKind {
@@ -73,8 +74,6 @@ struct IOOutput {
 IOOutput outputs_[4];
 
 
-void radioIrq() {
-}
 
 /** RTC ISR
  
@@ -85,35 +84,84 @@ ISR(RTC_PIT_vect) {
     status_.tick = true;
 }
 
-/** Checks if the appropriate output is a servo and if yes initiates the control pulse. 
- 
-    The pulse is between 0.5 and 2.5ms. 
+/** Motor Control
  */
-void servoTick() {
-    uint8_t i = status_.tickCounter;
-    if (outputs_[i].kind == OutputKind::Servo) {
-        TCB0.CTRLA &= ~TCB_ENABLE_bm;
-        //DDISP(0,0, "Servo    ");
-        //TCB0.CCMP = 1650 + (outputs_[i].value & 0xff) * 26;
-        TCB0.CCMP = 1964 + (outputs_[i].value & 0xff) * 31;
-        //TCB0.CCMP = 9821;
-        gpio::high(outputs_[i].pin);
-        TCB0.CNT = 0;
-        TCB0.CTRLA |= TCB_ENABLE_bm; 
+namespace motor {
+    void initialize() {
+        // TODO set TCD to 20kHz
+    }
+}
+
+/** Servo Control
+
+    One timer is enough for all 4 outputs since the servo expects a short pulse (0.5..2.5ms in the lego servo case) every 20 or so ms. RTC ticks are used to switch between outputs and TCB0 is used to precisely terminate the pulse. 
+
+    TCB0 interrupt simply puts the control pin low to end the servo control pulse.
+ 
+ */
+namespace servo {
+    void initialize() {
+        // initialize TCB0 which we use for the servo pulse timing
+        // assuming the clock frequency is 10Mhz, then CCMP should be in the range of 5000 to 25000 for 5 to 2.5ms pulse
+        TCB0.CTRLB = TCB_CNTMODE_INT_gc;
+        TCB0.INTCTRL = TCB_CAPT_bm;
+        TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc; // | TCB_ENABLE_bm;
+    }
+
+    void tick() {
+        uint8_t i = status_.tickCounter;
+        if (outputs_[i].kind == OutputKind::Servo) {
+            TCB0.CTRLA &= ~TCB_ENABLE_bm;
+            // currently 1964 .. 9821 for 0.5 to 2.5ms pulse duration
+            // TODO the real numbers are incorrect because wrong CLKDIV was used
+            TCB0.CCMP = 1964 + (outputs_[i].value & 0xff) * 31;
+            gpio::high(outputs_[i].pin);
+            TCB0.CNT = 0;
+            TCB0.CTRLA |= TCB_ENABLE_bm; 
+        }
     }
 }
 
 ISR(TCB0_INT_vect) {
+    gpio::low(outputs_[status_.tickCounter].pin);
     TCB0.INTFLAGS = TCB_CAPT_bm;
     TCB0.CTRLA &= ~TCB_ENABLE_bm;
-    gpio::low(outputs_[status_.tickCounter].pin);
+}
+
+/** Tone generation. 
+ */
+namespace audio {
+    void initialize() {
+    }
+}
+
+namespace radio {
+
+    void irq() {
+        status_.radioIrq = true;
+    }
+
+    void initialize() {
+        if (! nrf_.initialize("TEST2", "TEST1"))
+            leds.fill(Color::Red().withBrightness(32));
+        else
+            leds.fill(Color::Black());
+        leds.update();
+        gpio::input(NRF_IRQ_PIN);
+        attachInterrupt(digitalPinToInterrupt(NRF_IRQ_PIN), irq, FALLING);
+
+        nrf_.standby();
+        nrf_.enableReceiver();
+    }
+
+
 }
 
 
 void setup() {
     gpio::initialize();
     // flash the white LED to notify we are awake
-    leds.fill(Color::Red().withBrightness(32));
+    leds.fill(Color::White().withBrightness(32));
     leds.update();
     // wait for voltages to stabilize
     cpu::delay_ms(100);
@@ -124,6 +172,8 @@ void setup() {
     outputs_[3].pin = XR2_PIN;
     outputs_[0].kind = OutputKind::Servo;
     outputs_[0].value = 255;
+    outputs_[1].kind = OutputKind::Servo;
+    outputs_[1].value = 255;
 
     gpio::output(XL1_PIN);
 
@@ -136,34 +186,19 @@ void setup() {
     RTC.PITINTCTRL |= RTC_PI_bm; // enable the interrupt
     RTC.PITCTRLA = RTC_PERIOD_CYC128_gc + RTC_PITEN_bm;
     RTC.CTRLA |= RTC_RTCEN_bm;
-    // initialize TCB0 which we use for the servo pulse timing
-    // assuming the clock frequency is 10Mhz, then CCMP should be in the range of 5000 to 25000 for 5 to 2.5ms pulse
-    TCB0.CTRLB = TCB_CNTMODE_INT_gc;
-    TCB0.INTCTRL = TCB_CAPT_bm;
-    TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc; // | TCB_ENABLE_bm;
-    //TCB0.CCMP = 50000; // for 100Hz    
+
+    motor::initialize();
+    servo::initialize();
+    audio::initialize();
+
+    radio::initialize();
+
+
+
 
 
     DDISP_INITIALIZE();
 
-    if (! radio.initialize("TEST2", "TEST1")) {
-        DDISP(0,0,"Radio FAIL");
-    } else {
-        DDISP(0,0,"Radio OK");
-    }
-    gpio::input(NRF_IRQ_PIN);
-    attachInterrupt(digitalPinToInterrupt(NRF_IRQ_PIN), radioIrq, FALLING);
-
-    radio.standby();
-    radio.enableReceiver();
-
-    // start the 1kHz timer for ticks
-    // TODO change this to 8kHz for audio recording, or use different timer? 
-    TCB0.CTRLB = TCB_CNTMODE_INT_gc;
-    TCB0.INTCTRL = TCB_CAPT_bm;
-    TCB0.CTRLA = TCB_CLKSEL_CLKDIV2_gc | TCB_ENABLE_bm;
-    DDISP(0,0, "? Recvd  ");
-    DDISP(64,0, "Errors");
 }
 
 uint8_t x = 0;
@@ -172,10 +207,13 @@ void loop() {
     if (status_.tick) {
         status_.tick = false;
         ++status_.tickCounter;
-        servoTick();
+        servo::tick();
         if (++x == 0) {
             outputs_[0].value += 1;
         }
+    }
+    if (status_.radioIrq) {
+        // TODO
     }
     /*
     if (radio_irq) {
