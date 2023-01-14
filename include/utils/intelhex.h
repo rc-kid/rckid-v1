04bb7a621833549119979544a0d83b68899e3e4b
zduka <zduka@outlook.com>
2023-01-14 23:23:05 +0100

    intel hex reader

--- include/utils/intelhex.h
+++ include/utils/intelhex.h
@@ -5,6 +5,10 @@
 #include <istream>
 #include <sstream>
 
+#ifndef STR
+#define STR(__VA_ARGS__...) static_cast<std::stringstream &&>(std::stringstream() << __VA_ARGS__).str()
+#endif
+
 
 /** Intel HEX format support. 
  
@@ -12,14 +16,37 @@
  */
 namespace hex {
 
+    struct Error : std::invalid_argument {
+        size_t line;
+        size_t col;
+
+        Error(std::string const & str, size_t line, size_t col):
+            std::invalid_argument{str},
+            line{line},
+            col{col} {
+        }
+
+        friend std::ostream & operator << (std::ostream & s, Error const & e) {
+            s << e.what() << " (line " << e.line << ", col " << e.col << ")";
+            return s;
+        }
+
+    }; // hex::Error
+
     /** Record kind. 
      
         Data, end of file, or some of the address specifiers. 
      */
     enum class RecordKind : uint8_t {
+        /** Data. Up to 256 bytes per record is supported. 
+         */
         Data = 0, 
+        /** End of file. Required for proper parsing.
+         */
         EndOfFile = 1, 
         ExtendedSegmentAddress = 2, 
+        /** 4byte address where the code execution should start.
+         */
         StartSegmentAddress = 3,
         ExtendedLinearAddress = 4, 
         StartLinearAddress = 5,
@@ -40,9 +67,18 @@ namespace hex {
 
         RecordKind kind() const { return kind_; }
 
-        uint8_t length() const { return length_; }
+        size_t length() const { return length_; }
 
-        uint16_t address() const { return address_; }
+        size_t address() const { 
+            switch (kind_) {
+                case RecordKind::Data:
+                    return address_;
+                case RecordKind::StartSegmentAddress:
+                    return (data_[0] << 24) | (data_[1] << 16) | (data_[2] << 8) | data_[3];
+                default:
+                    assert(false && "Cannot take address of a given record kind");
+            }
+        }
 
         uint8_t const * data() const {
             assert(kind_ == RecordKind::Data && "Attempting to get data of a non-data record");
@@ -86,20 +122,23 @@ namespace hex {
                 r.data_ = new uint8_t[len];
                 r.length_ = len;
             }
-            uint16_t addr = static_cast<uint16_t>(parseByte()) << 8 | parseByte();
+            r.address_ = static_cast<uint16_t>(parseByte()) << 8 | parseByte();
             uint8_t k = parseByte();
             if (k > static_cast<uint8_t>(RecordKind::StartLinearAddress))
-                throw "Invalid record kind";
+                throw Error{STR("Invalid record kind: " << (int)k), l_, c_};
             r.kind_ = static_cast<RecordKind>(k);
             for (uint8_t i = 0; i < len; ++i) 
                 r.data_[i] = parseByte();
-            uint8_t checksum = crc_;
-            if (checksum != parseByte())
-                throw "Invalid CRC";
+            uint8_t crc = (~crc_ + 1) & 0xff;
+            uint8_t checksum = parseByte();
+            if (crc != checksum)
+                throw Error{STR("Invalid CRC: expected " << (int)crc << ", found " << (int)checksum), l_, c_};
             return true;
         }
 
     private:
+
+        friend class Program;
 
         void skipWhitespace() {
             while (! s_.eof()) {
@@ -120,7 +159,7 @@ namespace hex {
         }
 
         uint8_t parseByte() {
-            uint8_t result = fromHex(s_.get()) << 8 | fromHex(s_.get());
+            uint8_t result = (fromHex(s_.get()) << 4) | fromHex(s_.get());
             crc_ += result;
             return result;
         }
@@ -133,14 +172,14 @@ namespace hex {
             else if (x >= 'A' && x <= 'F')
                 return x - 'A' + 10;
             else
-                throw std::invalid_argument("Not a hex digit");
+                throw Error{STR("Not a hex digit: " << x << " (code: " << (int)x << ")"), l_, c_};
         }
 
         // location information for better errors
         size_t l_ = 1;
         size_t c_ = 1;
 
-        uint8_t crc_ = 0;
+        unsigned crc_ = 0;
 
         std::istream & s_;
     }; // hex::Record::Parser
@@ -162,26 +201,35 @@ namespace hex {
 
         static Program parse(std::istream & s) {
             auto parser = Record::Parser{s};
-            size_t c = 1024;
             Program p;            
             Record r;
+            size_t line = parser.l_;
+            size_t col = parser.c_;
             while (parser.parseRecord(r)) {
                 switch (r.kind()) {
                     case RecordKind::Data: {
-                        while (p.size + r.length() > c)
+                        while (p.size + r.length() > p.c_)
                             p.grow();
-                        if (r.address() != p.end())
-                            throw "Address mismatch";
+                        if (p.size == 0)
+                            p.address = r.address();
+                        else if (r.address() != p.end())
+                            throw Error{STR("Address mismatch: program expects " << p.end() << ", record specifies " << r.address()), line, col};
                         std::memcpy(p.data + p.size, r.data(), r.length());
                         p.size += r.length();
+                        break;
                     }
                     case RecordKind::EndOfFile:
                         return p;
+                    case RecordKind::StartSegmentAddress:
+                        assert(p.address = r.address());
+                        break;
                     default:
-                        throw "Unknown record type";
+                        throw Error{STR("Unsupported record type: " << (int) r.kind()), line, col};
                 }
+                line = parser.l_;
+                col = parser.c_;
             }
-            throw "No end of file record found.";
+            throw Error{"No end of file record found.", line, col};
         }
 
         static Program parse(char const * s) {
@@ -192,7 +240,7 @@ namespace hex {
     private:
 
         Program():
-            data{new uint8_t[c_]} {
+            data{new uint8_t[1024]} {
         }
 
         void grow() {
